ac8a6aa09b45940f5b5772908758f69ca511356d
MUNNA <munna7777777.1.0@gmail.com>
2026-09-04 23:31:34 +0530

    add test rest endpoint

---
+++ backend/src/main/java/com/example/codebasearchaeologist/CodebasearchaeologistApplication.java
@@ -0,0 +1,13 @@
+package com.example.codebasearchaeologist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodebasearchaeologistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodebasearchaeologistApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/codebasearchaeologist/CodebasearchaeologistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codebasearchaeologist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodebasearchaeologistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
